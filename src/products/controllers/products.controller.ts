2b4ff09c821a1be48efbe5f6158a6ce7f6df0704
gasparnd <gaspardolcemascolo@gmail.com>
2022-04-02 17:03:46 -0300

    extends jwt guard

--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -21,25 +21,29 @@ import {
 } from '../dtos/products.dtos';
 import { ProductsService } from './../services/products.service';
 import { MongoIdPipe } from 'src/common/mongo-id.pipe';
-import { AuthGuard } from '@nestjs/passport';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { Public } from 'src/auth/decorators/public.decorator';
 
-@UseGuards(AuthGuard('jwt'))
+@UseGuards(JwtAuthGuard)
 @ApiTags('products')
 @Controller('products')
 export class ProductsController {
   constructor(private productsService: ProductsService) {}
 
+  @Public()
   @Get()
   @ApiOperation({ summary: 'List of products' })
   getProducts(@Query() params: FilterProductsDto) {
     return this.productsService.findAll(params);
   }
 
+  @Public()
   @Get('filter')
   getProductFilter() {
     return `yo soy un filter`;
   }
 
+  @Public()
   @Get(':productId')
   @HttpCode(HttpStatus.ACCEPTED)
   getOne(@Param('productId', MongoIdPipe) productId: string) {
